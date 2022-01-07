208a66ccf20dc1f0e978b0b81709fe064d40b85c
sulavpradhan <sulavpradhan35@gmail.com>
2022-01-07 18:06:59 +0545

    few_css_changes_made

--- weatherApp.js
+++ weatherApp.js
@@ -3,6 +3,7 @@ var x = document.getElementById("location");
 var y = document.getElementById("temperature");
 var z = document.getElementById("feelsLike");
 var a = document.getElementById("weather");
+var i = document.getElementById("icon")
 var lat, long;
 function getLocation() {
     if (navigator.geolocation) {
@@ -21,16 +22,22 @@ function getLocation() {
      
     const display = async () => {
       let WeatherData = await weather
-      let location="location: "+WeatherData.timezone
-      let temperature ="temperature: "+ WeatherData.current.temp+"°C"
-      let feelsLike ="feelsLike: "+ WeatherData.current.feels_like+"°C"
-      // let CurrentWeather = "weather: "+ WeatherData.weather[0].description
+      console.log(WeatherData)
+      let location=WeatherData.timezone
+      let temperature = WeatherData.current.temp+"°C"
+      let feelsLike = "Feels Like: "+WeatherData.current.feels_like+"°"
+      let CurrentWeather =  WeatherData.current.weather[0].description
+      let IconCode = WeatherData.current.weather[0].icon
+      var Img = document.createElement("img")
+      Img.src = "http://openweathermap.org/img/wn/"+IconCode+"@2x.png"
+
   
 
       x.innerHTML=location
       y.innerHTML=temperature
       z.innerHTML=feelsLike
-      // a.innerHTML=CurrentWeather
+      a.innerHTML=CurrentWeather
+      i.appendChild(Img)
     }
     display()
     }
