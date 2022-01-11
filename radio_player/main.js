09e231cca0d2e4b090ba6963dce9d7a3276c1d48
sulavpradhan <sulavpradhan35@gmail.com>
2022-01-11 15:19:17 +0545

    button added

--- radio_player/main.js
+++ radio_player/main.js
@@ -5,11 +5,15 @@ const display = i=>{
     let markup = document.createElement("div");
     markup.innerHTML=`<div>
                             <h2>${data[i].name}</h2>
-                            <p><img src="${data[i].image}"></p>
-                            <p><a href="${data[i].url}">click to play</a></p>
+                            <p><a href="${data[i].url}"><button type="button">Click to Play!</button></a></p>
                         </div>`;
     DisplayData.appendChild(markup);
     
+}
+const PlayAudio = ()=>{
+    var x = document.getElementById("myAudio");
+    x.play();
+
 }
 for (var i=0; i<NoOfData; i++ ){
 
