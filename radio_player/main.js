8652430d18804b26bec0a9fa826edbafaa58320c
sulavpradhan <sulavpradhan35@gmail.com>
2022-01-12 19:05:48 +0545

    alternate image added

--- radio_player/main.js
+++ radio_player/main.js
@@ -5,6 +5,7 @@ const display = i=>{
     let markup = document.createElement("div");
     markup.innerHTML=`<div>
                             <h2>${data[i].name}</h2>
+                            <p><img src="${data[i].image}" onerror="this.src='https://cdn-icons-png.flaticon.com/512/3075/3075906.png';" ></p>
                             <p><audio src="${data[i].url}" controls preload> </audio></p>
                             
                         </div>`;
