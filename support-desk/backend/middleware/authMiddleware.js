8a718072d320245374f5a9445f542f1843ac59fe
sulavpradhan <sulavpradhan35@gmail.com>
2022-02-19 10:05:49 +0545

    auth api added

---
+++ support-desk/backend/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  console.log("before if");
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    console.log("inside if");
+
+    try {
+      //  Get token from header
+      token = req.headers.authorization.split(" ")[1];
+      // Verify the token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get user form token
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  console.log("after if");
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized");
+  }
+});
+
+module.exports = { protect };
