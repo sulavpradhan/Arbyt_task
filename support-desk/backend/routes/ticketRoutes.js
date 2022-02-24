ea77717ba76f478fab85224ac7b0f311a7a72c3c
sulavpradhan <sulavpradhan35@gmail.com>
2022-02-24 23:22:42 +0545

    diplay and submit note

--- support-desk/backend/routes/ticketRoutes.js
+++ support-desk/backend/routes/ticketRoutes.js
@@ -10,6 +10,10 @@ const {
 
 const { protect } = require("../middleware/authMiddleware");
 
+// Re-route to note router
+const noteRouter = require("./noteRoutes");
+router.use("/:id/notes", noteRouter);
+
 router.route("/").get(protect, getTickets).post(protect, createTickets);
 
 router
--- support-desk/frontend/src/pages/Ticket.jsx
+++ support-desk/frontend/src/pages/Ticket.jsx
@@ -1,16 +1,45 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Modal from "react-modal";
+import { FaPlus } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+import {
+  getNotes,
+  reset as notesReset,
+  createNote,
+} from "../features/notes/noteslice";
 import { useParams, useNavigate } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
+import NoteItem from "../components/NoteItem";
+
+const customStyles = {
+  content: {
+    width: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    position: "relative",
+  },
+};
+
+Modal.setAppElement("#root");
 
 function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.ticket
   );
+  const { notes, isLoading: notesIsLoading } = useSelector(
+    (state) => state.notes
+  );
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [noteText, setNoteText] = useState("");
 
   const params = useParams();
   const dispatch = useDispatch();
@@ -24,10 +53,11 @@ function Ticket() {
     }
 
     dispatch(getTicket(ticketId));
+    dispatch(getNotes(ticketId));
     // eslint-disable-next-line
   }, [isError, message, ticketId]);
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner />;
   }
 
@@ -39,6 +69,22 @@ function Ticket() {
     dispatch(closeTicket(ticketId));
     toast.success("Ticket Closed");
     navigate("/tickets");
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const onNoteSubmit = (e) => {
+    e.preventDefault();
+    const anyText = "anything";
+    dispatch(createNote({ noteText, ticketId }));
+    console.log(noteText);
+    closeModal();
   };
 
   return (
@@ -60,7 +106,45 @@ function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h3>Notes</h3>
       </header>
+      {ticket.status !== "closed" && (
+        <button onClick={openModal} className="btn">
+          <FaPlus /> Add Note
+        </button>
+      )}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Add Note"
+      >
+        {" "}
+        <h2>Add Note</h2>
+        <button className="btn-close" onClick={closeModal}>
+          X
+        </button>
+        <form onSubmit={onNoteSubmit}>
+          <div className="form-group">
+            <textarea
+              name="noteText"
+              id="noteText"
+              className="form-control"
+              placeholder="Note text"
+              value={noteText}
+              onChange={(e) => setNoteText(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button className="btn" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+      </Modal>
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
       {ticket.status !== "closed" && (
         <button onClick={onTicketClose} className="btn btn-block btn-danger">
           Close Ticket
